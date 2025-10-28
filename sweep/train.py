6398d3ecf2e3223a1cc549bb4a5c09b6f1770e38
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2025-10-28 16:13:19 -0700

    fix: update sweep.yaml to maximize val/accuracy instead of val_accuracy

--- sweep/train.py
+++ sweep/train.py
@@ -29,13 +29,13 @@ def main():
                                             download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, 
                                               batch_size=config.batch_size,
-                                              shuffle=True, num_workers=2)
+                                              shuffle=True, num_workers=4, pin_memory=True)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                            download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, 
                                              batch_size=config.batch_size,
-                                             shuffle=False, num_workers=2)
+                                             shuffle=False, num_workers=4, pin_memory=True)
 
     # 5. Define Model (ResNet18)
     model = torchvision.models.resnet18(pretrained=False, num_classes=10)
@@ -61,7 +61,7 @@ def main():
         model.train()
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
-            inputs, labels = data[0].to(device), data[1].to(device)
+            inputs, labels = data[0].to(device, non_blocking=True), data[1].to(device, non_blocking=True)
 
             optimizer.zero_grad()
             outputs = model(inputs)
@@ -80,7 +80,7 @@ def main():
         total = 0
         with torch.no_grad():
             for data in testloader:
-                images, labels = data[0].to(device), data[1].to(device)
+                images, labels = data[0].to(device, non_blocking=True), data[1].to(device, non_blocking=True)
                 outputs = model(images)
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
@@ -96,4 +96,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
