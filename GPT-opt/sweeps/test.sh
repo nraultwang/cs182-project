d47684dc8bd1ddecedbf689d7cb00d75e4872177
jasont314 <jasontrinh@berkeley.edu>
2025-11-21 17:06:24 -0800

    Remove obsolete sweep shell scripts

--- GPT-opt/sweeps/test.sh
+++
@@ -1,8 +0,0 @@
-WANDB="+logging_params.wandb.project=polar-express"
-COMMON="+training_data=shakespeare"
-MUON="optimizer_params.name=muon +optimizer_params.args.ns_steps=5"
-
-./submit_hydra.sh test_hydra $COMMON optimizer_params.name=adamw $WANDB
-./submit_hydra.sh test_hydra $COMMON $MUON +optimizer_params.args.polar_method=Keller $WANDB
-./submit_hydra.sh test_hydra $COMMON $MUON +optimizer_params.args.polar_method=Jiacheng $WANDB
-./submit_hydra.sh test_hydra $COMMON $MUON +optimizer_params.args.polar_method=polarexpress $WANDB
